33c8bb0de6bf48afa4865df96f5aba2203ec13d4
il-kow@mail.ru <il-kow@mail.ru>
2020-02-05 22:27:19 +0400

    upd(DocField) access level has been modified

--- app/src/Services/Constructor/Entities/DocField.php
+++ app/src/Services/Constructor/Entities/DocField.php
@@ -5,13 +5,13 @@ namespace App\src\Services\Constructor\Entities;
 
 class DocField extends AbstractField implements FieldInterface
 {
-    private $type = 'text';
-    
+    public $type = 'text';
+
     public function setDefaultValue()
     {
         return null;
     }
-    
+
     public function getType(): string
     {
         return $this->type;
