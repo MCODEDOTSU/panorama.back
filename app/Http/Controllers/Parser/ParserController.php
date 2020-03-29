7d39e8307644445c99a06eb758ae8a3174351215
il-kow@mail.ru <il-kow@mail.ru>
2020-03-29 11:26:33 +0400

    upd(ParserController)

--- app/Http/Controllers/Parser/ParserController.php
+++ app/Http/Controllers/Parser/ParserController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers\Parser;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class ParserController
 {
     public function parse(Request $request)
     {
+        try {
 
+            $inputFileName = 'C:\My\Work\OSPanel\domains\panorama.back\parser_example.xlsx';
+
+            $reader = new Xlsx();
+
+            $spreadsheet = $reader->load($inputFileName);
+
+            $this->readCells($spreadsheet);
+
+        } catch (Exception $e) {
+            return response('Something went wrong with file processing', 400);
+        }
+    }
+
+    private function readCells(SpreadSheet $spreadSheet)
+    {
+        $sheets = $spreadSheet->getAllSheets();
+
+        $firstSheet = $sheets[0];
+
+        $ppNumberCell = $firstSheet->getCell('B2');
+
+        dd($ppNumberCell->getValue());
     }
 }
