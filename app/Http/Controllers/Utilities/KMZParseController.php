242f6ee05ca1be5e10e043df24b77971bc73a3ff
il-kow@mail.ru <il-kow@mail.ru>
2020-05-08 18:10:52 +0400

    [feat/kmz]:(Service for parsing KML has been featured)

--- app/Http/Controllers/Utilities/KMZParseController.php
+++ app/Http/Controllers/Utilities/KMZParseController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Utilities;
 
 use App\Http\Controllers\Controller;
+use App\src\Utilities\KMZParserService;
 use Chumper\Zipper\Zipper;
+use Exception;
 use Illuminate\Http\Request;
-use ZipArchive;
 
 class KMZParseController extends Controller
 {
@@ -13,18 +14,33 @@ class KMZParseController extends Controller
     private const STORAGE_FOLDER = 'kmzs';
     private const KML_EXTRACTION_FOLDER = 'extracted/kmz';
 
+    private $kmzParserService;
+
+    /**
+     * KMZParseController constructor.
+     * @param KMZParserService $KMZParserService
+     */
+    public function __construct(KMZParserService $KMZParserService)
+    {
+        $this->kmzParserService = $KMZParserService;
+    }
+
     public function parse(Request $request)
     {
         // Upload file
         $kmzPath = $request->kmz->storeAs(self::STORAGE_FOLDER, self::KMZ_FILE_NAME);
 
         // unpack it
-        (new Zipper)->make('../storage/app/'.$kmzPath)->extractTo(self::KML_EXTRACTION_FOLDER);
-
-        // get kml
+        try {
+            (new Zipper)->make('../storage/app/' . $kmzPath)->extractTo(self::KML_EXTRACTION_FOLDER);
+        } catch (Exception $e) {
+            return response("KMZ file has not been found");
+        }
 
         // parse KML
+        $this->kmzParserService->parse(public_path() . '/extracted/kmz/doc.kml');
 
         return response("kmz has been processed");
     }
+
 }
