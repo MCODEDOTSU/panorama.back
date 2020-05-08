aebcd1e58cacf877f356d70dd99d603cb4dc1cde
il-kow@mail.ru <il-kow@mail.ru>
2020-05-08 16:54:52 +0400

    [feat/kmz]:(unzipping kmz file)

--- app/Http/Controllers/Utilities/KMZParseController.php
+++ app/Http/Controllers/Utilities/KMZParseController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers\Utilities;
 
 use App\Http\Controllers\Controller;
-use http\Env\Request;
+use Chumper\Zipper\Zipper;
+use Illuminate\Http\Request;
+use ZipArchive;
 
 class KMZParseController extends Controller
 {
+    private const KMZ_FILE_NAME = 'kmz.zip';
+    private const STORAGE_FOLDER = 'kmzs';
+    private const KML_EXTRACTION_FOLDER = 'extracted/kmz';
+
     public function parse(Request $request)
     {
         // Upload file
+        $kmzPath = $request->kmz->storeAs(self::STORAGE_FOLDER, self::KMZ_FILE_NAME);
 
-        // Unpack it
+        // unpack it
+        (new Zipper)->make('../storage/app/'.$kmzPath)->extractTo(self::KML_EXTRACTION_FOLDER);
 
-        // Load KML
+        // get kml
 
-        // Parse KML
+        // parse KML
+
+        return response("kmz has been processed");
     }
 }
