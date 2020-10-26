1aa4ee3125014dc62bcf14e30011ebd581e61aec
leliaoff <leliaoff@ya.ru>
2020-10-26 18:12:33 +0400

    fix

--- app/src/Services/PersonService.php
+++ app/src/Services/PersonService.php
@@ -109,6 +109,11 @@ class PersonService
         $thumbnail->fit(256, 256, function ($constraint) {
             $constraint->aspectRatio();
         });
+
+        if (!file_exists("storage/thumbnail/$path")) {
+            mkdir("storage/thumbnail/$path", 666, true);
+        }
+
         $thumbnail->save(public_path("storage/thumbnail/$path/$filename"));
 
         return [
